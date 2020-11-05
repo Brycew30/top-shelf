5edc87edd6a00170d22c3f7b818be189e3bdd2ab
Brycew30 <brycew30@gmail.com>
2020-11-05 15:42:57 -0600

    add routes

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :books
+      end
+      post 'login', action: :login, controller: 'users'
+    end
+  end
+
+  get 'api/v1/science', to: 'fetch_books#science'
+  get 'api/v1/nonfiction', to: 'fetch_books#nonfiction'
+  get 'api/v1/fiction', to: 'fetch_books#fiction'
 end
