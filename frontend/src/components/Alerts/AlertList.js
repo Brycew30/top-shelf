cc11fb0449cb59c68c6e4eb941743736a954e599
Brycew30 <brycew30@gmail.com>
2020-11-06 16:09:17 -0600

    create alert list

---
+++ frontend/src/components/Alerts/AlertList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import AlertMessage from './AlertMessage';
+import { connect } from 'react-redux';
+import { deleteAlertMessage } from '../../actions/alertActions';
+
+class AlertList extends Component {
+
+  removeAlert = (id) => {
+    this.props.deleteAlertMessage(id)
+  }
+
+  render() {
+    const alerts = this.props.alerts.map(alert => <AlertMessage key={alert.id} alert={alert} removeAlert={this.removeAlert}/>)
+    return (
+      <>
+        { alerts }
+      </>
+    );
+  }
+
+}
+
+const mapStateToProps = state => {
+  return {
+    alerts: state.alerts
+  }
+}
+
+export default connect(mapStateToProps, { deleteAlertMessage })(AlertList);
