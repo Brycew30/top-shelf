a3d95362247115667c7c2dfc19eec99daf9b6050
Brycew30 <brycew30@gmail.com>
2020-11-06 15:38:58 -0600

    create book card

---
+++ frontend/src/components/NYTbooks/NYTbookCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NYTbookCard = (props) =>
+  <>
+    <li
+    key={props.book.rank}
+    className="pborder"
+    >
+    <h3 onClick={() => props.cFd(props.book)}>#{props.book.rank} Bestseller</h3>
+    <h4 onClick={() => props.cFd(props.book)}>{props.book.title}</h4>
+    <h6 onClick={() => props.cFd(props.book)}>By: {props.book.author}</h6>
+    {props.book.weeks_on_list > 0 ? <h6 onClick={() => props.cFd(props.book)}>{props.book.weeks_on_list} week(s) on Bestseller List</h6> : null}
+    <p onClick ={() => props.cFd(props.book)}>Synopsis: {props.book.description}</p>
+    {props.book.review !== "" ?  <a href={props.book.review} target="_blank" rel="noopener noreferrer">Link to New York Times Book Review </a> : <a href={props.book.amazon_url} target="_blank" rel="noopener noreferrer">Link to Review</a>}
+  </li>
+  </>
+
+export default NYTbookCard;
