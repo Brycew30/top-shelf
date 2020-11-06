f2be5823dbc844cfe9b1039e022860a7ac0ccb30
Brycew30 <brycew30@gmail.com>
2020-11-06 15:03:27 -0600

    create AppRouter

---
+++ frontend/src/components/AppRouter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import App from '../App.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <>
+        <Route path="/" component={App}/>
+      </>
+    </Router>
+  )
+
+}
+
+export default AppRouter;
