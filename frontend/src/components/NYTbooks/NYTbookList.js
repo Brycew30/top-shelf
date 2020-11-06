0b6d89105207cef085a64c64207d50ded2ff038d
Brycew30 <brycew30@gmail.com>
2020-11-06 15:19:01 -0600

    create NYTbooks component

---
+++ frontend/src/components/NYTbooks/NYTbookList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import UserBooks from '../UserBooks/UserBooks';
+
+class NYTbookList extends Component {
+  state = {
+    show: false
+  }
+
+  componentDidMount() {
+    if(this.props.books.length === 0) {
+      this.props.getUserBooks(this.props.user)
+    }
+  }
+
+  toParent = (x) => {
+    this.setState({show: x})
+  }
+
+  render() {
+    const display = this.state.show === true ? {} : {display: "none"}
+    return (
+      <div className="bodymargin">
+        <ShowMyBooksButton toParent={this.toParent}/>
+        <UserBooks display={display} />
+        <hr/>
+        <BookGenreDropDown />
+        <BestSellers />
+      </div>
+    );
+  }
+
+}
+
+const mapStateToProps = state => {
+  return ({
+    books: state.user.books
+  })
+}
+
+export default connect(mapStateToProps)(NYTbookList);
