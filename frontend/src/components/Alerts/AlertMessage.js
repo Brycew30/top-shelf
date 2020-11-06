0faa86ee790b8961dae1f52924ef4e979953d517
Brycew30 <brycew30@gmail.com>
2020-11-06 15:59:13 -0600

    create alert messages

---
+++ frontend/src/components/Alerts/AlertMessage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './AlertMessage.css'
+
+class AlertMessage extends React.Component {
+
+  componentDidMount() {
+    this.timer = setTimeout(() =>
+      this.props.removeAlert(this.props.alert.id),
+      2000
+    )
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer)
+  }
+
+  render() {
+    const { alert } = this.props
+    const alertClassName = `${ alert.type }`
+    return (
+      <div className={alertClassName}>
+      { alert.text }
+      <button
+        onClick={ () => {this.props.removeAlert(alert.id)} }> x
+      </button>
+      </div>
+    );
+  }
+
+}
+
+export default AlertMessage;
