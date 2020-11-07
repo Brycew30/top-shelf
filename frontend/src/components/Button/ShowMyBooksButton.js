cce5df8d6ffc2f01321d2c3d7f307f540a10907f
Brycew30 <brycew30@gmail.com>
2020-11-06 18:17:00 -0600

    create button to see my books

---
+++ frontend/src/components/Button/ShowMyBooksButton.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './ShowMyBooksButton.css'
+
+class ShowMyBooksButton extends Component {
+  state = {
+    showMybooks: false,
+    buttonText: false
+  }
+
+  onShowClick = () => {
+    this.props.toParent(!this.state.showMybooks)
+    this.setState({
+      showMybooks: !this.state.showMybooks,
+      buttonText: !this.state.buttonText}
+    )
+  }
+
+  render() {
+    let buttonText = this.state.buttonText === true ? "- Hide My Books -" : "-Show My Books-"
+    const button = <h3 className="link" onClick={this.onShowClick}>{buttonText}</h3>
+    return (
+      <>
+      { button }
+      </>
+    );
+  }
+
+}
+
+export default ShowMyBooksButton;
