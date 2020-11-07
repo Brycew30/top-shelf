c5d8dc0b186073a73120b9d3c82167a94845efbc
Brycew30 <brycew30@gmail.com>
2020-11-06 18:10:05 -0600

    add imports and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import UserContainer from './containers/UserContainer';
+import NYTbookList from './components/NYTbooks/NYTbookList';
+import NavBarComponent from './components/NavBar/NavBarComponent';
+import BestSellers from './containers/BestSellers';
+import UserBooks from './components/UserBooks/UserBooks';
 import { connect } from 'react-redux';
 import { logOutUser, logInForRefresh } from './actions/UserActions';
 import { getUserBooks } from './actions/BookActions';
+import AlertList from './components/Alerts/AlertList';
 
 class App extends Component {
 
@@ -25,14 +30,21 @@ class App extends Component {
       this.props.logOutUser(this.props.user)
       return <Redirect to="/"/>
     }
-    // const logged = loggedIn() ? <NYTbookList user={this.props.user} getUserBooks={this.props.getUserBooks}/> : <UserContainer signUp={this.props.signUp}/>
-    // const nav = loggedIn() > <NavBar username={this.props.username} books={this.props.books} /> : null
+    const logged = loggedIn() ? <NYTbookList user={this.props.user} getUserBooks={this.props.getUserBooks}/> : <UserContainer signUp={this.props.signUp}/>
+    const nav = loggedIn() ? <NavBarComponent username={this.props.username} books={this.props.books} /> : null
     return (
       <div className="App">
+        {nav}
+        <AlertList />
         <Switch>
-        <Route exact path="/" render={() => <UserContainer signUp={this.props.signUp}/>} />
-        <Route path="/login" component={() => logout()} />
-        <Route path="/logout" component={() => logout()} />
+        <Route exact path="/" render={() => logged} />
+        <Route path="/bestsellers-fiction" component={() => !loggedIn() ? <Redirect to="/"/> : <BestSellers togglegenre="fiction"/> }/>
+        <Route path="/bestsellers-nonfiction" component={() => !loggedIn() ? <Redirect to="/"/> : <BestSellers togglegenre="nonfiction"/> }/>
+        <Route path="/bestsellers-science" component={() => !loggedIn() ? <Redirect to="/"/> : <BestSellers togglegenre="science"/> }/>
+        <Route exact path="/users/:id/books" render= {(routerProps) => !loggedIn() ? <Redirect to="/"/> : <UserBooks {...routerProps} /> }/>
+        <Route path="/login" component={ () => logout()} />
+        <Route path="/logout" component={ () => logout()} />
+        <Route render={() => <h1 className="error-404">404 Error - Page Not Found</h1>} />
         </Switch>
       </div>
     )
